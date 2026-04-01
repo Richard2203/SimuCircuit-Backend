8e05d5efe3408d3d5d23adfd507f529937f50cde
Lettu0309 <lechugacesarale@gmail.com>
2026-04-01 11:45:45 -0600

    Base de datos

---
+++ src/config/db.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+// Probamos la conexión al arrancar
+pool.getConnection()
+    .then(connection => {
+        console.log(`Base de datos del Simulador conectada en el puerto:${process.env.DB_PORT}`);
+        connection.release();
+    })
+    .catch(err => {
+        console.error('Error al conectar con la base de datos:', err.message);
+    });
+
+module.exports = pool;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const app = require('./app');
 
+// 1. Invocamos el archivo de configuración de la BD para que se ejecute la conexión a la misma
+require('./config/db');
+
 const PORT = process.env.SERVER_PORT || 3001;
 
+// 2. Invocamos al servidor para que escuche peticiones
 app.listen(PORT, () => {
     console.log(`🔌 Servidor corriendo en http://localhost:${PORT}`);
 });
